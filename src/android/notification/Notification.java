3b66908872ef7dd615795d316ae77d5277453c03
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-10-07 14:52:04 +0100

    refactor: refactor code for better readability
    References: https://outsystemsrd.atlassian.net/browse/RMET-3699

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -385,13 +385,11 @@ public final class Notification {
      * Present the local notification to user.
      */
     public void show() {
-
+        // Don't show notification if the application is in foreground and foreground option is false
         boolean showEvenInForeground = getOptions().getDict().optBoolean("foreground", false);
 
-        // Don't show notification if the application is in foreground and foreground option is false
-        if(applicationState == null || (applicationState.equalsIgnoreCase("foreground") && !showEvenInForeground)) {
+        if (applicationState == null || (applicationState.equalsIgnoreCase("foreground") && !showEvenInForeground))
             return;
-        }
 
         if (builder == null) return;
 
